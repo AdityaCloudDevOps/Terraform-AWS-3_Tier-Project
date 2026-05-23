1cf0456dabedad4e5fd10dbf1c8ecd6e2f06af31
Aditya Yadav <ay.aditya22@.com>
2026-05-23 17:13:51 +0530

    Updated professional README

--- Modules/Compute/main.tf
+++ Modules/Compute/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web_server" {
   ami                    = "ami-0f58b397bc5c1f2e8"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
 
